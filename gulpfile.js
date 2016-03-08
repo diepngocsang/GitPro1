8b806999996c35c42d8a8624fae2ab409e4d6284
Diep Ngoc Sang <sangdngc60231@gmail.com>
2016-03-08 22:54:15 +0700

    update_code
    Fix code + Bower + design + testing karma

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,24 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var browserSync = require('browser-sync').create();
-gulp.task('hello', function() {
-  console.log('Hello Zell');
-});
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
 gulp.task('sass', function(){
-  return gulp.src('scss/**/*.scss')
+  return gulp.src('my_scss/**/*.scss')
     .pipe(sass()) // Using gulp-sass
     .pipe(gulp.dest('my_css'))
 });
+gulp.task('uglify', function () {
+    return gulp.src('my_js/*.js')
+        .pipe(concat('sang.js'))
+        .pipe(gulp.dest('dist'))
+        .pipe(rename('sang.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist'));
+});
 gulp.task('watch',function(){
     gulp.watch('scss/**/*.scss',['sass']);
-})
+    gulp.watch('my_js/*.js', ['uglify']);
+});
+
+
